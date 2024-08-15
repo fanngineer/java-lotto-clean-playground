566163b65f0bf174cfc840b6c51b6c7d864f5a5c
김성환 <fanngineer@gmail.com>
2024-08-16 02:54:38 +0900

    refactoring

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,8 @@
 package view;
 
+import domain.LottoPaper;
+import domain.LottoResult;
 import domain.Rank;
-import dto.LottoPaperDto;
-import dto.LottoResultDto;
 import dto.RowDto;
 import java.util.Arrays;
 import java.util.List;
@@ -16,7 +16,7 @@ public class OutputView {
 
     public final List<Integer> REWARD = Arrays.asList(5000, 50000, 1500000, 2000000000);
 
-    public void printPaper(LottoPaperDto lottoPaper) {
+    public void printPaper(LottoPaper lottoPaper) {
         List<RowDto> rows = lottoPaper.getRows();
         for (int i = 0; i < lottoPaper.getRowNum(); i++) {
             System.out.println(rows.get(i).getNums().toString());
@@ -24,7 +24,7 @@ public class OutputView {
         System.out.println();
     }
 
-    public void printResult(LottoResultDto lottoResult) {
+    public void printResult(LottoResult lottoResult) {
         System.out.println("당첨 통계");
         System.out.println("---------");
         Map<Rank, Integer> resultMap = lottoResult.getResultMap();
@@ -45,7 +45,7 @@ public class OutputView {
         System.out.printf(" (%d원)- %d개\n", rank.getRewardMoney(), resultMap.get(rank));
     }
 
-    public void printRewardRate(LottoResultDto lottoResultDto) {
+    public void printRewardRate(LottoResult lottoResultDto) {
         System.out.println("총 수익률은 " + lottoResultDto.getRewardRate() + "입니다.");
     }
 }
